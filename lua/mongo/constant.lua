79c170eed420d61da39730026f0de81398ac8101
donus <donus33@gmail.com>
2024-08-18 15:56:27 +0700

    fix: remove constant state

--- lua/mongo/constant.lua
+++ lua/mongo/constant.lua
@@ -1,12 +1,5 @@
 local Constant = {}
 
-Constant.state = {
-  init = 1,
-  connected = 2,
-  db_selected = 3,
-  collection_selected = 4,
-}
-
 Constant.host_example = "/** mongodb://user:pass@localhost:27017/?authSource=admin */"
 Constant.host_fallback = "mongodb://localhost:27017"
 Constant.mongodb_crud_page = "https://www.mongodb.com/docs/manual/crud/"
